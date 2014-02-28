1400a5f9747dff38a8772a458d12ddbe3c73d15c
Andy Berry <andyberry88@gmail.com>
2014-02-28 16:26:08 +0000

    using tweaked version of browser modules that prioritises the module.exports code path in the UMD header

--- target/single/fell-no-emitr.js
+++ target/single/fell-no-emitr.js
@@ -1,105 +1,100 @@
-// fell built for browser 2013-11-06T16:47:23.865Z
+// fell built for browser 2014-02-28T16:25:45.438Z
 ;(function (name, factory) {
-	if (typeof define === 'function') {
-		// A system that provides a define method that takes a name and a factory, e.g. AMD
-		define(name, factory);
-	} else if (typeof exports === 'object') {
-		// Node. Does not work with strict CommonJS, but
-		// only CommonJS-like enviroments that support module.exports,
-		// like Node.
+	if (typeof module === 'object') {
+		// Does not work with strict CommonJS, but only CommonJS-like environments
+		// that support module.exports - like Node.
 		module.exports = factory();
+	} else if (typeof define === 'function') {
+		define(factory);
 	} else {
+		// For an environment with no require/define framework loaded - e.g. a browser.
 		var global = Function("return this")();
 		global[name] = factory();
 	}
-})( "fell", function() {
+})
+( "fell", function() {
 	var global = Function("return this")();
 	
-	var _define = global.define; // avoid triggering wrapped umd modules.
-	var require = global.require;
+	function realm(parentRequire) {
+		var moduleDefinitions = {}, incompleteExports = {}, moduleExports = {}, modulesFromParent = {};
 	
-	if (typeof _define !== 'function' || typeof require !== 'function') {
-		function realm(parentRequire) {
-			var moduleDefinitions = {}, incompleteExports = {}, moduleExports = {}, modulesFromParent = {};
-	
-			function derelativise(context, path) {
-				var result = (context === "" || path.charAt(0) !== '.') ? [] : context.split("/");
-				var working = path.split("/"), item;
-				while (item = working.shift()) {
-					if (item === "..") {
-						result.pop();
-					} else if (item !== ".") {
-						result.push(item);
-					}
+		function derelativise(context, path) {
+			var result = (context === "" || path.charAt(0) !== '.') ? [] : context.split("/");
+			var working = path.split("/"), item;
+			while (item = working.shift()) {
+				if (item === "..") {
+					result.pop();
+				} else if (item !== ".") {
+					result.push(item);
 				}
-				return result.join("/");
 			}
-	
-			function define(id, definition) {
-				if (id in moduleDefinitions) {
-					throw new Error('Module ' + id + ' has already been defined.');
-				}
-				if (modulesFromParent[id] === true) {
-					throw new Error('Module ' + id + ' has already been loaded from a parent realm.');
-				}
-				moduleDefinitions[id] = definition;
-			}
-	
-			function require(context, id) {
-				id = derelativise(context, id).replace(/\.js$/, "");
-	
-				if (moduleExports[id] != null) { return moduleExports[id]; }
-	
-				if (incompleteExports[id] != null) {
-					// there is a circular dependency, we do the best we can in the circumstances.
-					return incompleteExports[id].exports;
-				}
-	
-				var definition = moduleDefinitions[id];
-				if (definition == null) {
-					if (parentRequire != null) {
-						var result = parentRequire(id);
-						modulesFromParent[id] = true;
-						return result;
-					}
-					throw new Error("No definition for module " + id + " has been loaded.");
-				}
-	
-				var module = { exports: {}, id: id };
-				incompleteExports[id] = module;
-				try {
-					if (typeof definition === 'function') {
-						var definitionContext = id;
-						var idx = id.lastIndexOf("/");
-						if (idx >= 0) {
-							definitionContext = id.substring(0, idx);
-						}
-						var returnValue = definition.call(module, require.bind(null, definitionContext), module.exports, module);
-						moduleExports[id] = returnValue || module.exports;
-					} else {
-						moduleExports[id] = definition;
-					}
-				} finally {
-					delete incompleteExports[id];
-				}
-				return moduleExports[id];
-			}
-	
-			return {
-				define: define, require: require.bind(null, '')
-			};
+			return result.join("/");
 		}
 	
-		var defaultRealm = realm(global.require || function(moduleId) {
-			if (global[moduleId]) {
-				return global[moduleId];
+		function define(id, definition) {
+			if (id in moduleDefinitions) {
+				throw new Error('Module ' + id + ' has already been defined.');
 			}
-			throw new Error("No definition for module " + moduleId + " could be found in the global top level.");
-		});
+			if (modulesFromParent[id] === true) {
+				throw new Error('Module ' + id + ' has already been loaded from a parent realm.');
+			}
+			moduleDefinitions[id] = definition;
+		}
 	
-		require = defaultRealm.require;
-		_define = defaultRealm.define;
+		function require(context, id) {
+			id = derelativise(context, id).replace(/\.js$/, "");
+	
+			if (moduleExports[id] != null) { return moduleExports[id]; }
+	
+			if (incompleteExports[id] != null) {
+				// there is a circular dependency, we do the best we can in the circumstances.
+				return incompleteExports[id].exports;
+			}
+	
+			var definition = moduleDefinitions[id];
+			if (definition == null) {
+				if (parentRequire != null) {
+					var result = parentRequire(id);
+					modulesFromParent[id] = true;
+					return result;
+				}
+				throw new Error("No definition for module " + id + " has been loaded.");
+			}
+	
+			var module = { exports: {}, id: id };
+			incompleteExports[id] = module;
+			try {
+				if (typeof definition === 'function') {
+					var definitionContext = id;
+					var idx = id.lastIndexOf("/");
+					if (idx >= 0) {
+						definitionContext = id.substring(0, idx);
+					}
+					var returnValue = definition.call(module, require.bind(null, definitionContext), module.exports, module);
+					moduleExports[id] = returnValue || module.exports;
+				} else {
+					moduleExports[id] = definition;
+				}
+			} finally {
+				delete incompleteExports[id];
+			}
+			return moduleExports[id];
+		}
+	
+		return {
+			define: define, require: require.bind(null, '')
+		};
 	}
+	
+	var defaultRealm = realm(global.require || function(moduleId) {
+		if (global[moduleId]) {
+			return global[moduleId];
+		}
+		throw new Error("No definition for module " + moduleId + " could be found in the global top level.");
+	});
+	
+	var require = defaultRealm.require;
+	var _define = defaultRealm.define;
 
 	_define("fell", function(require, exports, module) {
 		module.exports = require("./lib/fell");
--- target/system/fell.js
+++ target/system/fell.js
@@ -1,4 +1,4 @@
-// fell built for bundle module system 2013-11-06T16:47:23.865Z
+// fell built for bundle module system 2014-02-28T16:25:45.438Z
 define("fell", function(require, exports, module) {
 	module.exports = require("./lib/fell");
 });
@@ -186,123 +186,6 @@ define("fell/lib/Utils", function(require, exports, module) {
 		allowAll: allowAll
 	};
 });
-define("fell/lib/Log", function(require, exports, module) {
-	"use strict";
-	
-	var Emitter = require('emitr');
-	var Logger = require('./Logger');
-	var Levels = require('./Levels');
-	
-	var DEFAULT_COMPONENT = "[default]";
-	
-	function Log() {
-		this.loggers = null;
-		this.config = null;
-		this.defaultLevel = null;
-		this.rootLogger = null;
-		this.clear();
-	}
-	Emitter.mixInto(Log);
-	
-	Log.prototype.DEFAULT_COMPONENT = DEFAULT_COMPONENT;
-	
-	Log.prototype.getLogger = function(component) {
-		if (arguments.length === 0) { component = DEFAULT_COMPONENT; }
-		if (this.loggers[component] !== undefined) {
-			return this.loggers[component];
-		}
-		var logger = new Logger(this, component);
-		var level = bestLevelMatch(this.config, component, this.defaultLevel);
-		logger._setLevel(level);
-		this.loggers[component] = logger;
-	
-		return logger;
-	};
-	
-	Log.prototype.configure = function(defaultLevel, config, destinations) {
-		if (arguments.length === 1 && typeof defaultLevel === 'object') {
-			config = arguments[0];
-			defaultLevel = "info";
-		}
-		config = config || {};
-		this.config = config;
-		this.defaultLevel = defaultLevel;
-		setLoggerLevels(defaultLevel, config, this.loggers);
-		this.rootLogger = this.getLogger();
-	
-		this.off();
-		if (! destinations && typeof console !== 'undefined') {
-			var ConsoleLogDestination = require('./destination/ConsoleLog');
-			destinations = [new ConsoleLogDestination()];
-		}
-		destinations = destinations || [];
-	
-		for (var i = 0; i < destinations.length; ++i) {
-			this.addDestination(destinations[i]);
-		}
-	};
-	
-	Log.prototype.changeLevel = function(component, level) {
-		if (arguments.length === 1) {
-			this.defaultLevel = arguments[0];
-		} else {
-			this.config[component] = level;
-		}
-		setLoggerLevels(this.defaultLevel, this.config, this.loggers);
-		this.rootLogger = this.getLogger();
-	};
-	
-	Log.prototype.addDestination = function(logDestination, context) {
-		if (context === undefined && typeof logDestination !== 'function') {
-			context = logDestination;
-			logDestination = context.onLog;
-		}
-		this.on('log', logDestination, context);
-	};
-	
-	Log.prototype.removeDestination = function(logDestination, context) {
-		if (arguments.length === 1 && typeof logDestination !== 'function') {
-			context = logDestination;
-			logDestination = context.onLog;
-		}
-		this.off('log', logDestination, context);
-	};
-	
-	Log.prototype.clear = function() {
-		this.loggers = {};
-		this.configure("info");
-	};
-	
-	Log.prototype.Levels = Levels;
-	
-	Levels.forEach(function(level) {
-		Log.prototype[level.toUpperCase()] = level;
-	
-		// Convenience methods to log to the root logger.
-		Log.prototype[level] = function() {
-			this.rootLogger[level].apply(this.rootLogger, arguments);
-		};
-	});
-	
-	function setLoggerLevels(defaultLevel, config, loggers) {
-		for (var loggerId in loggers) {
-			var level = bestLevelMatch(config, loggerId, defaultLevel);
-			loggers[loggerId]._setLevel(level);
-		}
-	}
-	
-	function bestLevelMatch(config, key, otherwise) {
-		var candidates = Object.keys(config)
-				.filter(function(a) {
-					return a === key || key.substring(0, a.length + 1) === (a + ".");
-				}).sort(function(a, b) {
-					return b.length - a.length;
-				});
-		return candidates[0] ? config[candidates[0]] : otherwise;
-	}
-	
-	module.exports = new Log();
-});
 define("fell/lib/RingBuffer", function(require, exports, module) {
 	"use strict";
 	
@@ -481,6 +364,123 @@ define("fell/lib/RingBuffer", function(require, exports, module) {
 	RingBuffer.errorMessage = errorMessage;
 	
 	module.exports = RingBuffer;
+});
+define("fell/lib/Log", function(require, exports, module) {
+	"use strict";
+	
+	var Emitter = require('emitr');
+	var Logger = require('./Logger');
+	var Levels = require('./Levels');
+	
+	var DEFAULT_COMPONENT = "[default]";
+	
+	function Log() {
+		this.loggers = null;
+		this.config = null;
+		this.defaultLevel = null;
+		this.rootLogger = null;
+		this.clear();
+	}
+	Emitter.mixInto(Log);
+	
+	Log.prototype.DEFAULT_COMPONENT = DEFAULT_COMPONENT;
+	
+	Log.prototype.getLogger = function(component) {
+		if (arguments.length === 0) { component = DEFAULT_COMPONENT; }
+		if (this.loggers[component] !== undefined) {
+			return this.loggers[component];
+		}
+		var logger = new Logger(this, component);
+		var level = bestLevelMatch(this.config, component, this.defaultLevel);
+		logger._setLevel(level);
+		this.loggers[component] = logger;
+	
+		return logger;
+	};
+	
+	Log.prototype.configure = function(defaultLevel, config, destinations) {
+		if (arguments.length === 1 && typeof defaultLevel === 'object') {
+			config = arguments[0];
+			defaultLevel = "info";
+		}
+		config = config || {};
+		this.config = config;
+		this.defaultLevel = defaultLevel;
+		setLoggerLevels(defaultLevel, config, this.loggers);
+		this.rootLogger = this.getLogger();
+	
+		this.off();
+		if (! destinations && typeof console !== 'undefined') {
+			var ConsoleLogDestination = require('./destination/ConsoleLog');
+			destinations = [new ConsoleLogDestination()];
+		}
+		destinations = destinations || [];
+	
+		for (var i = 0; i < destinations.length; ++i) {
+			this.addDestination(destinations[i]);
+		}
+	};
+	
+	Log.prototype.changeLevel = function(component, level) {
+		if (arguments.length === 1) {
+			this.defaultLevel = arguments[0];
+		} else {
+			this.config[component] = level;
+		}
+		setLoggerLevels(this.defaultLevel, this.config, this.loggers);
+		this.rootLogger = this.getLogger();
+	};
+	
+	Log.prototype.addDestination = function(logDestination, context) {
+		if (context === undefined && typeof logDestination !== 'function') {
+			context = logDestination;
+			logDestination = context.onLog;
+		}
+		this.on('log', logDestination, context);
+	};
+	
+	Log.prototype.removeDestination = function(logDestination, context) {
+		if (arguments.length === 1 && typeof logDestination !== 'function') {
+			context = logDestination;
+			logDestination = context.onLog;
+		}
+		this.off('log', logDestination, context);
+	};
+	
+	Log.prototype.clear = function() {
+		this.loggers = {};
+		this.configure("info");
+	};
+	
+	Log.prototype.Levels = Levels;
+	
+	Levels.forEach(function(level) {
+		Log.prototype[level.toUpperCase()] = level;
+	
+		// Convenience methods to log to the root logger.
+		Log.prototype[level] = function() {
+			this.rootLogger[level].apply(this.rootLogger, arguments);
+		};
+	});
+	
+	function setLoggerLevels(defaultLevel, config, loggers) {
+		for (var loggerId in loggers) {
+			var level = bestLevelMatch(config, loggerId, defaultLevel);
+			loggers[loggerId]._setLevel(level);
+		}
+	}
+	
+	function bestLevelMatch(config, key, otherwise) {
+		var candidates = Object.keys(config)
+				.filter(function(a) {
+					return a === key || key.substring(0, a.length + 1) === (a + ".");
+				}).sort(function(a, b) {
+					return b.length - a.length;
+				});
+		return candidates[0] ? config[candidates[0]] : otherwise;
+	}
+	
+	module.exports = new Log();
 });
 define("fell/lib/destination/LogStore", function(require, exports, module) {
 	"use strict";
